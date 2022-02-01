3f2e2a5af8d0c91fdba478dd5cd31d65d44e1876
NEO <singhmanhasnitin@gmail.com>
2022-02-01 16:15:36 +0530

    test3

--- app/src/main/java/com/example/musiast/MainActivity.kt
+++ app/src/main/java/com/example/musiast/MainActivity.kt
@@ -7,11 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    //test1
-    //hello
-        //hola
-        //hello2
-        //hi
-        //nitin
-        //12345
-    }}
+    }
+}
